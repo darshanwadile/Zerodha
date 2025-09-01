4a61c56f8a9e9f60cd6b257f78971dfe7ac95636
Darshan Wadile <darshanwadile10@gmail.com>
2025-09-01 21:09:44 +0530

    Fixed model issue

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,7 @@ const authRoute = require("./Routes/AuthRoute");
 const { authenticateUser } = require("./Middlewares/AuthMiddleware");
 
 // Configure CORS to allow credentials from specific origins
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(cookieParser());
